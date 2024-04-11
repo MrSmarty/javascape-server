cbffaba65cae1234530160b9551dd37346355ded
Lincoln Smith <MrSmartyMcFarty@gmail.com>
2024-04-11 14:48:10 -0500

    Documented the EditableLabel class

--- src/main/java/com/javascape/ui/EditableLabel.java
+++ src/main/java/com/javascape/ui/EditableLabel.java
@@ -9,11 +9,36 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 
+/**
+ * A class for creating editable labels. The labels can be double clicked to
+ * enter an editing menu. nce the editing is done, hit the enter key to save the
+ * value.
+ * <p>
+ * TODO: Save the value on loss of focus
+ */
 public class EditableLabel extends StackPane {
 
+    /** The Label element for the EditableLabel */
     Label label;
+    /** The TextField element for the EditableLabel */
     TextField field;
 
+    /**
+     * Constructor for the EditableLabel class. This constructor creates a new
+     * editable label using the given text. The parent object is the object that
+     * will be used to "call" the method (generally just 'this'). The method is the
+     * method to call for saving the new value.
+     * 
+     * <p>
+     * Example: EditableLabel nameLabel = new EditableLabel(getName(), this,
+     * this.getClass().getMethod("setName", String.class));
+     * 
+     * @param text   The text to display on the label when intialized.
+     * @param parent The object that will be used to call the method. Generally
+     *               'this'.
+     * @param method The method to call for saving the new value. Must contain only
+     *               a single String parameter.
+     */
     public EditableLabel(String text, Object parent, Method method) {
         label = new Label(text);
         field = new TextField(text);
